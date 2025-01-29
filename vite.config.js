5565fa3642e2551c8ca69d14e726017b8b6b24e5
JiteshKkumar <jiteshraj6313@gmail.com>
2025-01-29 23:19:48 +0530

    need some change to run on host

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // need to remove later
+  server: {
+    allowedHosts: ['https://digitallyindia.onrender.com'],
+  },
+  
 })
 
 // export default {
